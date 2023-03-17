fb05d3c491df37d19a19636e1ef86799a4dab9c5
Mayur <64288579+mayur6161@users.noreply.github.com>
2023-03-17 19:56:48 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
 import logo from './logo.svg';
 import './App.css';
+import MovieData from './components/moviedata.json'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (< >
+        <h1 className="App">Movie Shop</h1>
+
+        <div style={{height: "40px", width: "100%"}}></div>
+
+        <div style={{marginLeft: "110px"}}>
+            {MovieData.map((data, key) => {
+                return (<div style={{width: "33%", float: "left"}} key={key}>
+                    <img style={{width: "70%", height: "400px", alignContent: "space-between"}}
+                         src={data.Poster} alt={data.Title}/>
+                    <p style={{fontSize: "22px", fontWeight: "bold", color: "green"}}>{data.Title}</p>
+                    <div style={{height: "40px", width: "100%",}}></div>
+
+                </div>);
+            })}
+        </div>
+
+        <div style={{marginLeft: "110px"}}>
+            {MovieData.map((data, key) => {
+                return (<div style={{width: "33%", float: "left"}} key={key}>
+                    <img style={{width: "70%", height: "400px", alignContent: "space-between"}}
+                         src={data.Poster} alt={data.Title}/>
+                    <p style={{fontSize: "22px", fontWeight: "bold", color: "green"}}>{data.Title}</p>
+                    <div style={{height: "40px", width: "100%",}}></div>
+
+                </div>);
+            })}
+        </div>
+
+
+
+
+    </>);
 }
+
+
+
+
+
 
 export default App;
